267b2b7121645a7f363b18b86810e63e2fd101a7
deqnchugunov <deqnchugunov@gmail.com>
2014-04-10 22:57:03 +0300

    Create header.php

---
+++ views/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Style-Type" content="text/css" />
+        <meta http-equiv="Content-Style-Type" content="text/css" />
+        <link type="text/css" rel="stylesheet" href="./css/style.css" />
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+        <script language="javascript" src="./js/main.js"></script>
+        <script language="javascript" src="./js/dropdown.js"></script>
+        <title><?= $page_title; ?></title>
+    </head>
+    <body>
+        <div id="wrapper">
