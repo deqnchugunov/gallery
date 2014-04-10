c266893e22cf3430ffd69da3edc6d09db2d8ba23
deqnchugunov <deqnchugunov@gmail.com>
2014-04-10 22:42:34 +0300

    Create BaseModel.php

---
+++ system/BaseModel.php
@@ -0,0 +1,21 @@
+<?php
+
+class BaseModel {
+
+    private static $connection = null;
+    protected $db_connection = null;
+
+    public function __construct() {
+        if (self::$connection == null) {
+            $config = Config::getInstance();
+            self::$connection = new mysqli($config->getConfig('db_host'), $config->getConfig('db_user'), $config->getConfig('db_pass'), $config->getConfig('db_database'), $config->getConfig('db_port'));
+            if (self::$connection->connect_error) {
+                die('Connection Error');
+            }
+            self::$connection->set_charset("utf8");
+        }
+        mb_internal_encoding('utf-8');
+        $this->db_connection = self::$connection;
+    }
+
+}
