5e8d263a1e58a25f88d609e37a5e2e2fbfc67a2a
deqnchugunov <deqnchugunov@gmail.com>
2014-04-10 22:45:05 +0300

    Create View.php

---
+++ system/View.php
@@ -0,0 +1,26 @@
+<?php
+
+class View {
+
+    private static $instance = null;
+    private $data = array();
+
+    private function __construct() {
+        
+    }
+
+    public function render($template, $data = array()) {
+        if (is_array($data)) {
+            extract($data);
+        }
+        include './views/' . $template . '.php';
+    }
+
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new View();
+        }
+        return self::$instance;
+    }
+
+}
